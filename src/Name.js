fea67802cd8628d9aa5d90440c4cdcf7211f1321
vimalverma558 <kumarvimal558@gmail.com>
2020-10-01 09:09:49 +0530

    socialMedia added

--- src/Name.js
+++ src/Name.js
@@ -5,7 +5,13 @@ const greeting = {
       "subTitle subTitle subTitle subTitle subTitle subTitle subTitle subTitle subTitle subTitle subTitle.",
   };
 const socialMedia = {
-    
+    facebook: "https://www.facebook.com/vimalverma558",
+    twitter: "https://www.facebook.com/vimalverma558",
+    instagram: "https://www.facebook.com/vimalverma558",
+    github: "https://www.facebook.com/vimalverma558",
+    gitlab: "https://www.facebook.com/vimalverma558",
+    devto: "https://www.facebook.com/vimalverma558",
+    mailto: "mailto:vimalverma558@live.com"
 }
 
 
